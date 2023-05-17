c8e59da3e5c1c7145ea050fce265ecdb9b8d0d2e
Gabriel Favaro <89041502+favarogabriel@users.noreply.github.com>
2023-05-17 00:17:38 -0300

    Initial commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x + 18, transform.position.y, player.transform.position.z);
+    }
+}
